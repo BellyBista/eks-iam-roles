a7b18669f8430898b4c0aeb4dfece3b4d6fb89e3
quadribello <quadribello41@gmail.com>
2024-01-06 21:26:59 -0500

    update variable.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "node_group_name" {
   type = string
   default = "eks-node-group-nodes"
-  description = "(Optional) The name of the role policy. If omitted, Terraform will assign a random, unique name."
+  description = "(Optional) The name of the node role policy. If omitted, Terraform will assign a random, unique name."
 }
 
 variable "cluster_role_name" {
   type = string
   default = "eks-cluster"
-  description = "(Optional) The name of the role policy. If omitted, Terraform will assign a random, unique name."
+  description = "(Optional) The name of the cluster role policy. If omitted, Terraform will assign a random, unique name."
 }
