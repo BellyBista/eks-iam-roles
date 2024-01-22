a90f384ff07bd1722866f98940ad5fc197f0a7e2
quadribello <quadribello41@gmail.com>
2024-01-22 16:30:05 -0500

    chore: added aws credentials

--- oidc/main.tf
+++ oidc/main.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "git_aws_oidc" {
     condition {
       test     = "StringLike"
       variable = "${replace(data.aws_iam_openid_connect_provider.git.url, "https://", "")}:sub"
-      values   = ["repo:quadri-olamilekan/*:*"]
+      values   = ["repo:quadri-olamilekan/eks-iam-roles:*"]
     }
 
 
