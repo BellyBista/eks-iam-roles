c6fe1969966fc03cbbd6337c75ff1e8f343f5f79
quadribello <quadribello41@gmail.com>
2024-01-21 21:45:50 -0500

    test example tfvars

---
+++ tests/test_example.py
@@ -0,0 +1,24 @@
+import os
+import tftest
+import pytest
+
+
+tfvars = ["/Users/quadribello/IAM-module/examples/complete/example.tfvars"]
+
+
+@pytest.fixture(params=tfvars)
+def plan(request, directory="/Users/quadribello/IAM-module/examples", module_name="complete"):
+    tfvars_file = request.param
+    tf = tftest.TerraformTest(module_name, directory)
+    tf.setup()
+    plan = tf.plan(
+        output=True, use_cache=True, tf_var_file=tfvars_file
+    ) 
+    return plan
+
+def test_variables(plan):
+    tf_vars = plan.variables
+    assert "region" in tf_vars
+    assert "pgp_key" in tf_vars
+    assert "developer" in tf_vars
+    assert "admin" in tf_vars
