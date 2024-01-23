56198b159724d8004d8d6a3105e6bad0e0ad80a9
quadribello <quadribello41@gmail.com>
2024-01-22 20:55:58 -0500

    chore: test apply

--- examples/complete/example.tfvars
+++ examples/complete/example.tfvars
@@ -1,4 +1,4 @@
 region    = "us-east-1"
-pgp_key   = ""
+pgp_key   = "keybase:quadribello41"
 developer = ["developer1", "developer2"]
 admin     = ["admin1"]
--- test_example.py
+++ test_example.py
@@ -28,3 +28,33 @@ def test_outputs(plan):
     assert "cluster_role" in plan.outputs
     assert "developer_password" in plan.outputs
     assert "admin_password" in plan.outputs
+
+@pytest.fixture(params=tfvars)
+def apply_output(request, directory="./examples", module_name="complete"):
+    tfvars_file = request.param
+    tf = tftest.TerraformTest(module_name, directory)
+    tf.setup()
+    tf.apply(output=True, use_cache=True, tf_var_file=tfvars_file)
+    output = tf.output()
+    yield output
+    tf.destroy(auto_approve=True, use_cache=True, tf_var_file=tfvars_file)
+
+
+def test_apply(apply_output):
+    admin_password = apply_output["admin_password"]
+    cluster_role = apply_output["cluster_role"]
+    developer_password = apply_output["developer_password"]
+    node_role = apply_output["node_role"]
+
+    assert isinstance(admin_password, dict)
+    assert isinstance(cluster_role, str)
+    assert isinstance(developer_password, dict)
+    assert isinstance(node_role, str)
+
+    expected_admin_users = ["admin1"]
+    for user in expected_admin_users:
+        assert user in admin_password
+
+    expected_developer_users = ["developer1", "developer2"]
+    for user in expected_developer_users:
+        assert user in developer_password
