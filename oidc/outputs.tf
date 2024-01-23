f6f65ec1e8085d5dd5f88e9881eee846d53377c7
quadribello <quadribello41@gmail.com>
2024-01-23 13:20:43 -0500

    chore: clean repo

--- oidc/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "git_actions_oidc" {
-  value = aws_iam_role.git_action.arn
-}
--- oidc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "url" {
-  type = string
-  description = "The actual URL of your existing provider"
-}
